4264da11a9ad54f13884591a283b325696a29a72
Idan Gabdank <gabdank@stanford.edu>
2017-07-20 09:34:59 -0700

    updating validator script and image

--- src/validate_output.py
+++ src/validate_output.py
@@ -4,17 +4,16 @@ import glob
 import sys
 
 KNOWN_FILES_MD5 = {
-    "rep1ENCFF000VOLchr21.raw.srt.filt.nodup.srt.final." +
-    "filt.nodup.sample.15.SE.tagAlign.gz.cc.qc":
-    "fdb8f09f8815576da28f1b00f0b1cb85",
-    "rep1ENCFF000VOLchr21.raw.srt.dup.qc":
-    "4994de20a2bf58a9b651bb4137a78db2",
-    "rep1ENCFF000VOLchr21.raw.srt.filt.nodup.srt.final.pbc.qc":
-    "fffb748b255a44f00c79c9a676575420",
-    "rep1ENCFF000VOLchr21.raw.srt.bam.flagstat.qc":
-    "82068728db28533fd764f12709f2b405",
-    "rep1ENCFF000VOLchr21.raw.srt.filt.nodup.srt.final.flagstat.qc":
-    "82068728db28533fd764f12709f2b405"}
+    'rep1_ENCFF000VOL_chr21.raw.srt.bam.flagstat.qc':
+        '13557cf731440268fc780bb0f17bb8ba',
+    'rep1_ENCFF000VOL_chr21.raw.srt.filt.nodup.srt.final.pbc.qc':
+        'fffb748b255a44f00c79c9a676575420',
+    'rep1_ENCFF000VOL_chr21.raw.srt.filt.nodup.srt.final.flagstat.qc':
+        '13557cf731440268fc780bb0f17bb8ba',
+    'rep1_ENCFF000VOL_chr21.raw.srt.filt.nodup.srt.final.filt.nodup.sample.15.SE.tagAlign.gz.cc.qc':
+        '9b373f2d5bb5a73cb74b24cf4f9bb132',
+    'rep1_ENCFF000VOL_chr21.raw.srt.dup.qc':
+        'eef880e26bbae594766e32e470a7d01e'}
 
 
 def calculatemd5FromFile(filepath, chunksize=4096):
