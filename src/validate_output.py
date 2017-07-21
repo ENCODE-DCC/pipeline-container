01f4d2f4d29548327dd660da568668eb30a535fc
Idan Gabdank <gabdank@stanford.edu>
2017-07-21 12:07:07 -0700

    added error messages to the validator script

--- src/validate_output.py
+++ src/validate_output.py
@@ -2,6 +2,7 @@ import json
 import hashlib
 import glob
 import sys
+from pprint import pprint
 
 KNOWN_OUTPUT_VALUES = {
     'crop_length': 'native',
@@ -41,28 +42,34 @@ def main():
     post_mapping_dict = {}
     filter_dict = {}
     xcor_dict = {}
-    with open(sys.argv[1]+'/' + mapping_output_json) as data:
-        mapping_dict = json.loads(data)
-    with open(sys.argv[1]+'/' + post_mapping_output_json) as data:
-        post_mapping_dict = json.loads(data)
-    with open(sys.argv[1]+'/' + filter_output_json) as data:
-        filter_dict = json.loads(data)
-    with open(sys.argv[1]+'/' + xcor_output_json) as data:
-        xcor_dict = json.loads(data)
 
-    result_dict = {'steps': {}, 'overall': True}
+    with open(sys.argv[1]+'/' + mapping_output_json) as data:
+        mapping_dict = json.load(data)
+    with open(sys.argv[1]+'/' + post_mapping_output_json) as data:
+        post_mapping_dict = json.load(data)
+    with open(sys.argv[1]+'/' + filter_output_json) as data:
+        filter_dict = json.load(data)
+    with open(sys.argv[1]+'/' + xcor_output_json) as data:
+        xcor_dict = json.load(data)
+
+    result_dict = {'steps': {}, 'overall': True, 'messages': ''}
+
     keep_examination = True
     if not mapping_dict:
         result_dict['steps']['mapping_step'] = False
+        result_dict['messages'] += 'No output from mapping step was found. '
         keep_examination = False
     if not post_mapping_dict:
         result_dict['steps']['post_mapping_step'] = False
+        result_dict['messages'] += 'No output from post-mapping step was found. '
         keep_examination = False
     if not filter_dict:
         result_dict['steps']['filtering_step'] = False
+        result_dict['messages'] += 'No output from filtering step was found. '
         keep_examination = False
     if not xcor_dict:
         result_dict['steps']['xcor_step'] = False
+        result_dict['messages'] += 'No output from xcor step was found. '
         keep_examination = False
     
 
@@ -78,52 +85,73 @@ def main():
         
         if mapping_dict.get('crop_length') == KNOWN_OUTPUT_VALUES['crop_length']:
             result_dict['steps']['mapping_step']['crop_length'] = True
-        else
+        else:
+            result_dict['messages'] += 'The crop length reported \'{}\', '.format(mapping_dict.get('crop_length')) + \
+                                       'is different from the expected value of \'{}\'. '.format(KNOWN_OUTPUT_VALUES['crop_length'])
             result_dict['overall'] = False
         
         if mapping_dict.get('paired_end') == KNOWN_OUTPUT_VALUES['paired_end']:
             result_dict['steps']['mapping_step']['paired_end'] = True
-        else
+        else:
+            result_dict['messages'] += 'Reported paired end boolean \'{}\' '.format(mapping_dict.get('paired_end')) + \
+                                       'is different from the expected value of \'{}\'. '.format(KNOWN_OUTPUT_VALUES['paired_end'])
             result_dict['overall'] = False
 
         if post_mapping_dict.get('n_mapped_reads') == KNOWN_OUTPUT_VALUES['n_mapped_reads']:
             result_dict['steps']['post_mapping_step']['n_mapped_reads'] = True
-        else
+        else:
+            result_dict['messages'] += 'Reported number of mapped reads \'{}\' '.format(mapping_dict.get('n_mapped_reads')) + \
+                                       'is different from the expected number of mapped reads \'{}\'. '.format(KNOWN_OUTPUT_VALUES['n_mapped_reads'])
             result_dict['overall'] = False
 
         if filter_dict.get('PBC1') == KNOWN_OUTPUT_VALUES['PBC1']:
             result_dict['steps']['filtering_step']['PBC1'] = True
-        else
+        else:
+            result_dict['messages'] += 'Reported PCR Bottlenecking Coefficient 1 \'{}\' '.format(mapping_dict.get('PBC1')) + \
+                                       'is different from the expected value of \'{}\'. '.format(KNOWN_OUTPUT_VALUES['PBC1'])
             result_dict['overall'] = False
         if filter_dict.get('PBC2') == KNOWN_OUTPUT_VALUES['PBC2']:
             result_dict['steps']['filtering_step']['PBC2'] = True
-        else
+        else:
+            result_dict['messages'] += 'Reported PCR Bottlenecking Coefficient 2 \'{}\' '.format(mapping_dict.get('PBC2')) + \
+                                       'is different from the expected value of \'{}\'. '.format(KNOWN_OUTPUT_VALUES['PBC2'])
             result_dict['overall'] = False
         if filter_dict.get('NRF') == KNOWN_OUTPUT_VALUES['NRF']:
             result_dict['steps']['filtering_step']['NRF'] = True
-        else
+        else:
+            result_dict['messages'] += 'Reported Non-Redindant Fraction \'{}\' '.format(mapping_dict.get('NRF')) + \
+                                       'is different from the expected value of \'{}\'. '.format(KNOWN_OUTPUT_VALUES['NRF'])
             result_dict['overall'] = False
         if filter_dict.get('duplicate_fraction') == KNOWN_OUTPUT_VALUES['duplicate_fraction']:
             result_dict['steps']['filtering_step']['duplicate_fraction'] = True
-        else
+        else:
+            result_dict['messages'] += 'Reported duplicate fraction \'{}\' '.format(mapping_dict.get('duplicate_fraction')) + \
+                                       'is different from the expected value of \'{}\'. '.format(KNOWN_OUTPUT_VALUES['duplicate_fraction'])
             result_dict['overall'] = False
 
 
         if xcor_dict.get('NSC') == KNOWN_OUTPUT_VALUES['NSC']:
             result_dict['steps']['xcor_step']['NSC'] = True
-        else
+        else:
+            result_dict['messages'] += 'Reported Normalized Strand Cross-correlation coefficient \'{}\' '.format(mapping_dict.get('est_frag_len')) + \
+                                       'is different from the expected value of \'{}\'. '.format(KNOWN_OUTPUT_VALUES['est_frag_len'])
             result_dict['overall'] = False        
         if xcor_dict.get('RSC') == KNOWN_OUTPUT_VALUES['RSC']:
             result_dict['steps']['xcor_step']['RSC'] = True
-        else
+        else:
+            result_dict['messages'] += 'Reported Relative Strand Cross-correlation coefficient \'{}\' '.format(mapping_dict.get('est_frag_len')) + \
+                                       'is different from the expected value of \'{}\'. '.format(KNOWN_OUTPUT_VALUES['est_frag_len'])
             result_dict['overall'] = False 
         if xcor_dict.get('est_frag_len') == KNOWN_OUTPUT_VALUES['est_frag_len']:
             result_dict['steps']['xcor_step']['est_frag_len'] = True
-        else
+        else:
+            result_dict['messages'] += 'Reported extimated fragment length \'{}\' '.format(mapping_dict.get('est_frag_len')) + \
+                                       'is different from the expected estimated fragment length \'{}\'. '.format(KNOWN_OUTPUT_VALUES['est_frag_len'])
             result_dict['overall'] = False
 
     with open('results.json', 'w') as f:
         json.dump(result_dict, f)
+    pprint(result_dict)
 
 if __name__ == "__main__":
     main()
