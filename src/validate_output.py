e204b4060695f61ffb6eef564d0f77ff2712b69b
Idan Gabdank <gabdank@stanford.edu>
2017-07-21 21:49:06 -0700

    updated the validatior script

--- src/validate_output.py
+++ src/validate_output.py
@@ -1,6 +1,4 @@
 import json
-import hashlib
-import glob
 import sys
 from pprint import pprint
 
@@ -18,20 +16,7 @@ KNOWN_OUTPUT_VALUES = {
     'est_frag_len': 120 # 125
 }
 
-def calculatemd5FromFile(filepath, chunksize=4096):
-    '''calculate md5sum of a file in filepath.
-        do the calculation in chunks of 4096
-        bytes as a memory efficiency consideration.'''
-    hash_md5 = hashlib.md5()
-    with open(filepath, 'rb') as f:
-        # Iter is calling f.read(chunksize) until it returns
-        # the sentinel b''(empty bytes)
-        for chunk in iter(lambda: f.read(chunksize), b''):
-            hash_md5.update(chunk)
-    return hash_md5.hexdigest()
-
 def main():
-    output_md5 = dict()
     path_to_output_folder = sys.argv[1]
 
     mapping_output_json = 'mapping.json'
@@ -87,6 +72,7 @@ def main():
         if mapping_dict.get('crop_length') == KNOWN_OUTPUT_VALUES['crop_length']:
             result_dict['steps']['mapping_step']['crop_length'] = True
         else:
+            result_dict['steps']['mapping_step']['crop_length'] = False
             result_dict['messages'] += 'The crop length reported \'{}\', '.format(mapping_dict.get('crop_length')) + \
                                        'is different from the expected value of \'{}\'. '.format(KNOWN_OUTPUT_VALUES['crop_length'])
             result_dict['overall'] = False
@@ -94,6 +80,7 @@ def main():
         if mapping_dict.get('paired_end') == KNOWN_OUTPUT_VALUES['paired_end']:
             result_dict['steps']['mapping_step']['paired_end'] = True
         else:
+            result_dict['steps']['mapping_step']['paired_end'] = False
             result_dict['messages'] += 'Reported paired end boolean \'{}\' '.format(mapping_dict.get('paired_end')) + \
                                        'is different from the expected value of \'{}\'. '.format(KNOWN_OUTPUT_VALUES['paired_end'])
             result_dict['overall'] = False
@@ -101,32 +88,37 @@ def main():
         if post_mapping_dict.get('n_mapped_reads') == KNOWN_OUTPUT_VALUES['n_mapped_reads']:
             result_dict['steps']['post_mapping_step']['n_mapped_reads'] = True
         else:
-            result_dict['messages'] += 'Reported number of mapped reads \'{}\' '.format(mapping_dict.get('n_mapped_reads')) + \
+            result_dict['steps']['post_mapping_step']['n_mapped_reads'] = False
+            result_dict['messages'] += 'Reported number of mapped reads \'{}\' '.format(post_mapping_dict.get('n_mapped_reads')) + \
                                        'is different from the expected number of mapped reads \'{}\'. '.format(KNOWN_OUTPUT_VALUES['n_mapped_reads'])
             result_dict['overall'] = False
 
         if filter_dict.get('PBC1') == KNOWN_OUTPUT_VALUES['PBC1']:
             result_dict['steps']['filtering_step']['PBC1'] = True
         else:
-            result_dict['messages'] += 'Reported PCR Bottlenecking Coefficient 1 \'{}\' '.format(mapping_dict.get('PBC1')) + \
+            result_dict['steps']['filtering_step']['PBC1'] = False
+            result_dict['messages'] += 'Reported PCR Bottlenecking Coefficient 1 \'{}\' '.format(filter_dict.get('PBC1')) + \
                                        'is different from the expected value of \'{}\'. '.format(KNOWN_OUTPUT_VALUES['PBC1'])
             result_dict['overall'] = False
         if filter_dict.get('PBC2') == KNOWN_OUTPUT_VALUES['PBC2']:
             result_dict['steps']['filtering_step']['PBC2'] = True
         else:
-            result_dict['messages'] += 'Reported PCR Bottlenecking Coefficient 2 \'{}\' '.format(mapping_dict.get('PBC2')) + \
+            result_dict['steps']['filtering_step']['PBC2'] = False
+            result_dict['messages'] += 'Reported PCR Bottlenecking Coefficient 2 \'{}\' '.format(filter_dict.get('PBC2')) + \
                                        'is different from the expected value of \'{}\'. '.format(KNOWN_OUTPUT_VALUES['PBC2'])
             result_dict['overall'] = False
         if filter_dict.get('NRF') == KNOWN_OUTPUT_VALUES['NRF']:
             result_dict['steps']['filtering_step']['NRF'] = True
         else:
-            result_dict['messages'] += 'Reported Non-Redindant Fraction \'{}\' '.format(mapping_dict.get('NRF')) + \
+            result_dict['steps']['filtering_step']['NRF'] = False
+            result_dict['messages'] += 'Reported Non-Redindant Fraction \'{}\' '.format(filter_dict.get('NRF')) + \
                                        'is different from the expected value of \'{}\'. '.format(KNOWN_OUTPUT_VALUES['NRF'])
             result_dict['overall'] = False
         if filter_dict.get('n_filtered_mapped_reads') == KNOWN_OUTPUT_VALUES['n_filtered_mapped_reads']:
             result_dict['steps']['filtering_step']['n_filtered_mapped_reads'] = True
         else:
-            result_dict['messages'] += 'Reported number of filtered mapped reads \'{}\' '.format(mapping_dict.get('n_filtered_mapped_reads')) + \
+            result_dict['steps']['filtering_step']['n_filtered_mapped_reads'] = False
+            result_dict['messages'] += 'Reported number of filtered mapped reads \'{}\' '.format(filter_dict.get('n_filtered_mapped_reads')) + \
                                        'is different from the expected number of filtered mapped reads \'{}\'. '.format(KNOWN_OUTPUT_VALUES['n_filtered_mapped_reads'])
             result_dict['overall'] = False
         
@@ -134,7 +126,8 @@ def main():
         if filter_dict.get('duplicate_fraction') == KNOWN_OUTPUT_VALUES['duplicate_fraction']:
             result_dict['steps']['filtering_step']['duplicate_fraction'] = True
         else:
-            result_dict['messages'] += 'Reported duplicate fraction \'{}\' '.format(mapping_dict.get('duplicate_fraction')) + \
+            result_dict['steps']['filtering_step']['duplicate_fraction'] = False
+            result_dict['messages'] += 'Reported duplicate fraction \'{}\' '.format(filter_dict.get('duplicate_fraction')) + \
                                        'is different from the expected value of \'{}\'. '.format(KNOWN_OUTPUT_VALUES['duplicate_fraction'])
             result_dict['overall'] = False
 
@@ -142,19 +135,22 @@ def main():
         if xcor_dict.get('NSC') == KNOWN_OUTPUT_VALUES['NSC']:
             result_dict['steps']['xcor_step']['NSC'] = True
         else:
-            result_dict['messages'] += 'Reported Normalized Strand Cross-correlation coefficient \'{}\' '.format(mapping_dict.get('est_frag_len')) + \
+            result_dict['steps']['xcor_step']['NSC'] = False
+            result_dict['messages'] += 'Reported Normalized Strand Cross-correlation coefficient \'{}\' '.format(xcor_dict.get('est_frag_len')) + \
                                        'is different from the expected value of \'{}\'. '.format(KNOWN_OUTPUT_VALUES['est_frag_len'])
             result_dict['overall'] = False        
         if xcor_dict.get('RSC') == KNOWN_OUTPUT_VALUES['RSC']:
             result_dict['steps']['xcor_step']['RSC'] = True
         else:
-            result_dict['messages'] += 'Reported Relative Strand Cross-correlation coefficient \'{}\' '.format(mapping_dict.get('est_frag_len')) + \
+            result_dict['steps']['xcor_step']['RSC'] = False
+            result_dict['messages'] += 'Reported Relative Strand Cross-correlation coefficient \'{}\' '.format(xcor_dict.get('est_frag_len')) + \
                                        'is different from the expected value of \'{}\'. '.format(KNOWN_OUTPUT_VALUES['est_frag_len'])
             result_dict['overall'] = False 
         if xcor_dict.get('est_frag_len') == KNOWN_OUTPUT_VALUES['est_frag_len']:
             result_dict['steps']['xcor_step']['est_frag_len'] = True
         else:
-            result_dict['messages'] += 'Reported extimated fragment length \'{}\' '.format(mapping_dict.get('est_frag_len')) + \
+            result_dict['steps']['xcor_step']['est_frag_len'] = False
+            result_dict['messages'] += 'Reported extimated fragment length \'{}\' '.format(xcor_dict.get('est_frag_len')) + \
                                        'is different from the expected estimated fragment length \'{}\'. '.format(KNOWN_OUTPUT_VALUES['est_frag_len'])
             result_dict['overall'] = False
 
